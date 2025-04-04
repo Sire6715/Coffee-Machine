0708096ace8956b0b3179d632c6566af2c14c69d
Prince <uchepibe@gmail.com>
2025-04-04 17:39:02 +0100

    fix: fixed if-condition for drink production

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ resources = {
 #TODO prompt user.
 machine_on = True
 
+
 water = resources["water"] 
 milk = resources["milk"] 
 coffee = resources["coffee"] 
@@ -42,8 +43,9 @@ coffee = resources["coffee"]
 
 
 while machine_on:
-    user_input =  input("What would you like? (espresso/Latte/Capaccino)").lower()
+    user_input =  input("What would you like? (espresso/Latte/capaccino)").lower()
                 
+   
     if user_input == "off":
         exit()
         machine_on = False
@@ -51,14 +53,18 @@ while machine_on:
     elif user_input == "report":
         for x in resources:
             print(f"{x}: {resources[x]}")
+            
     elif user_input in MENU:
         drink_ingredients = MENU[user_input]["ingredients"]  
-        
-        if drink_ingredients.get("water", 0) < water:
-            print("Sorry there is not enough water.")
-        elif drink_ingredients.get("milk", 0) < milk:
+        water_test = drink_ingredients.get("water", 0)
+        print(water)
+               
+        if  water < drink_ingredients.get("water"):
+            print("Sorry there is not enough water......###")
+            print(water_test)
+        elif milk < drink_ingredients.get("milk"):
             print("Sorry there is not enough milk.")
-        elif drink_ingredients.get("coffee", 0) < coffee:
+        elif coffee < drink_ingredients.get("coffee") :
             print("sorry there is not enough coffee")     
         else:
             for item in drink_ingredients:
